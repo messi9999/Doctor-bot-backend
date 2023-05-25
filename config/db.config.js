d1861219cfb7ebc82eab3672a0ae7f6174ae869e
super2058 <superMark2058@gmail.com>
2023-05-24 21:44:15 -0400

    env update

--- config/db.config.js
+++ config/db.config.js
@@ -5,7 +5,7 @@ module.exports = {
   USER: process.env.DB_USER || "root",
   PASSWORD: process.env.DB_PASSWORD || "12345",
   DB: process.env.DB_NAME || "newDatabase",
-  dialect: process.env.DB_DIALECT || "mysql2",
+  dialect: process.env.DB_DIALECT || "mariadb",
   pool: {
     max: 5,
     min: 0,
