0354e3849b78307052c3304d270722bb538d780e
super2058 <superMark2058@gmail.com>
2023-05-26 01:35:01 -0400

    test deploy

--- config/db.config.js
+++ config/db.config.js
@@ -1,15 +1,15 @@
 require("dotenv").config();
 
 module.exports = {
-  HOST: process.env.DB_HOST || "localhost",
+  HOST: process.env.DB_HOST || "127.0.0.1",
   USER: process.env.DB_USER || "root",
   PASSWORD: process.env.DB_PASSWORD || "12345",
-  DB: process.env.DB_NAME || "newDatabase",
+  DB: process.env.DB_NAME || "doctor",
   dialect: process.env.DB_DIALECT || "mariadb",
   pool: {
     max: 5,
     min: 0,
     acquire: 30000,
-    idle: 10000
-  }
+    idle: 10000,
+  },
 };
--- models/index.js
+++ models/index.js
@@ -4,13 +4,6 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
   host: config.HOST,
   dialect: config.dialect,
-  operatorsAliases: false,
-  pool: {
-    max: config.pool.max,
-    min: config.pool.min,
-    acquire: config.pool.acquire,
-    idle: config.pool.idle
-  }
 });
 
 const db = {};
@@ -24,12 +17,12 @@ db.role = require("./role.model.js")(sequelize, Sequelize);
 db.role.belongsToMany(db.user, {
   through: "user_roles",
   foreignKey: "roleId",
-  otherKey: "userId"
+  otherKey: "userId",
 });
 db.user.belongsToMany(db.role, {
   through: "user_roles",
   foreignKey: "userId",
-  otherKey: "roleId"
+  otherKey: "roleId",
 });
 
 db.ROLES = ["user", "admin"];
